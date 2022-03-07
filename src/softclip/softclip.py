a1c323f43909be9a0b06c92def12077786cb4377
yonesuke <13e.e.c.13@gmail.com>
2022-03-07 17:12:23 +0900

    rename function name

--- src/softclip/softclip.py
+++ src/softclip/softclip.py
@@ -10,25 +10,25 @@ class SoftClip:
 
         if self.low is not None and self.high is not None:
             assert self.low < self.high, f"low must be smaller than high, but your input is low({low}) > high({high}))"
-            self.transform_fn = lambda x: self.low + (self.high - self.low) * nn.sigmoid(x / self.hinge_softness)
+            self.forward_fn = lambda x: self.low + (self.high - self.low) * nn.sigmoid(x / self.hinge_softness)
             self.inverse_fn = lambda y: self.hinge_softness * jnp.log(y - self.low) - self.hinge_softness * jnp.log(self.high - y)
         
         elif self.low is not None and self.high is None:
-            self.transform_fn = lambda x: self.hinge_softness * nn.softplus(x / self.hinge_softness) + self.low
+            self.forward_fn = lambda x: self.hinge_softness * nn.softplus(x / self.hinge_softness) + self.low
             self.inverse_fn = lambda y: self.hinge_softness * jnp.log(jnp.exp((y - self.low) / self.hinge_softness) - 1.0)
         
         elif self.low is None and self.high is not None:
-            self.transform_fn = lambda x: -self.hinge_softness * nn.softplus(x / self.hinge_softness) + self.high
+            self.forward_fn = lambda x: -self.hinge_softness * nn.softplus(x / self.hinge_softness) + self.high
             self.inverse_fn = lambda y: self.hinge_softness * jnp.log(jnp.exp((self.high - y) / self.hinge_softness) - 1.0)
         
         else:
-            self.transform_fn = lambda x: x
+            self.forward_fn = lambda x: x
             self.inverse_fn = lambda y: y
 
     def forward(self, x):
-        return self.transform_fn(x)
+        return self.forward_fn(x)
 
-    def backward(self, y):
+    def inverse(self, y):
         return self.inverse_fn(y)
 
     def __repr__(self):
